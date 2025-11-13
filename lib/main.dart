9e6d897f587d8da5d62831749ac38e1bb76fb6b0
Febrian Arka Samudra <febrianarka10@gmail.com>
2025-11-13 12:44:13 +0700

    question8

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+// removed unused package import
 
 void main() {
   runApp(const MyApp());
@@ -122,20 +122,27 @@ class _FuturePageState extends State<FuturePage> {
   }*/
 
   void returnFG() {
-    FutureGroup<int> futureGroup = FutureGroup<int>();
-    futureGroup.add(returnOneAsync());
-    futureGroup.add(returnTwoAsync());
-    futureGroup.add(returnThreeAsync());
-    futureGroup.close();
-    futureGroup.future.then((List<int> value) {
-      int total = 0;
-      for (var element in value) {
-        total += element;
-      }
-      setState(() {
-        result = total.toString();
-      });
-    });
+    final futures = Future.wait<int>([
+      returnOneAsync(),
+      returnTwoAsync(),
+      returnThreeAsync(),
+    ]);
+
+    futures
+        .then((List<int> value) {
+          int total = 0;
+          for (var element in value) {
+            total += element;
+          }
+          setState(() {
+            result = total.toString();
+          });
+        })
+        .catchError((_) {
+          setState(() {
+            result = 'An error occurred';
+          });
+        });
   }
 
   @override
